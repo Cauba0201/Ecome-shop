f9337cffffa7db03813b2c8ef259ab5e4e8dd614
Cauba0201 <91552618+Cauba0201@users.noreply.github.com>
2025-12-22 14:59:48 +0700

    Create: Heading component

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,11 @@
 import Hero from "@/components/Hero";
+import IntroducingSection from "@/components/IntroducingSection";
 
 export default function Home() {
   return (
     <>
       <Hero />
-      <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-        <h1>Ecomerce Shop</h1>
-      </div>
+      <IntroducingSection />
     </>
   );
 }
